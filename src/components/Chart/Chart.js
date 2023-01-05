05005e52921b49e5b6c573b22c1b370e77d51065
Milton de la Rosa <miltondlr2000@gmail.com>
2023-01-05 13:13:37 -0600

    version 1.2

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value); //Transform an object to just a number
+
+  const totalMaximum = Math.max(...dataPointValues); //Spreed operator to pull out all the array elements and add them as standalone arguments
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+
+//Expenses Function.
+const Expenses = (props) => {
+  //Filter Use State declaration.
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  //Filter Change Handler use to view the filtered year
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onSelected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
